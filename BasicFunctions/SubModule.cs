c67105367ba09f8f5b2dd5c8cdd4cf30da20b927
frees <frees@PC>
2022-09-02 23:41:33 +0800

    添加项目文件。

---
+++ BasicFunctions/SubModule.cs
@@ -0,0 +1,14 @@
+﻿using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace BasicFunctions
+{
+    public class SubModule :MBSubModuleBase
+    {
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=5Ru6DbNctrkjzDs}Load Baisc Functions").ToString(), Color.White));
+        }
+    }
+}
---
+++ ChineseWeapons/SubModule.cs
@@ -0,0 +1,14 @@
+﻿using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace ChineseWarband
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=5Ru6DbNctrkjzDs}Load Cinsese Weapons").ToString(), Color.White));
+        }
+    }
+}
